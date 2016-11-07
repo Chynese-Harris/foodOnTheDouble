4d93a5a5592d257778b9d03b9b0b84e47a3f21c4
Chynese Harris <Chynese-Harris@users.noreply.github.com>
2016-11-07 11:31:25 -0600

    Add files via upload

---
+++ SimpleTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  SimpleTableViewController.h
+//  foodOnTheDouble
+//
+//  Created by mad mac on 10/11/16.
+//  Copyright (c) 2016 Treehouse. All rights reserved.
+//
+
+#ifndef foodOnTheDouble_SimpleTableViewController_h
+#define foodOnTheDouble_SimpleTableViewController_h
+
+
+#endif
+
+#import <UIKit/UIKit.h>
+@interface SimpleTableViewController : UIViewcontroller <UITableViewDelegate>,
+@end
---
+++ SimpleTableViewController.m
@@ -0,0 +1,20 @@
+//
+//  SimpleTableViewController.m
+//  foodOnTheDouble
+//
+//  Created by mad mac on 10/11/16.
+//  Copyright (c) 2016 Treehouse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@implementation SimpleTableViewController
+{
+    NSArray *recipes;
+
+}
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    recipes = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto",
+}
